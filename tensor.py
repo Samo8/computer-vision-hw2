b4b19bfd14f3ad50c7a175706358d7715c701cce
Adam Harnusek <adam.harnusek@gmail.com>
2021-11-25 16:25:24 +0200

    Pascal VOC annotation format

--- tensor.py
+++ tensor.py
@@ -4,14 +4,16 @@ import tensorflow_datasets as tfds
 import prepare_masks as prep
 import utils
 
-dataset = prep.loadImagesAndMasks()
+data = prep.loadImagesAndMasks()
 
+
+dataset = tf.data.TFRecordDataset()
 
 # dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
-train_images = dataset[:5].map(utils.load_image, num_parallel_calls=tf.data.AUTOTUNE)
-test_images = dataset[4:].map(utils.load_image, num_parallel_calls=tf.data.AUTOTUNE)
+# train_images = dataset[:5].map(utils.load_image, num_parallel_calls=tf.data.AUTOTUNE)
+# test_images = dataset[4:].map(utils.load_image, num_parallel_calls=tf.data.AUTOTUNE)
 
-print(train_images)
+# print(train_images)
 
 # print(info)
 # print(dataset)
